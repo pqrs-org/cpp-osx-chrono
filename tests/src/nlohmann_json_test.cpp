e45d4d0b9a6a5f878872af0e8a9335ea8abe92d8
Takayama Fumihiko <tekezo@pqrs.org>
2019-02-02 12:30:04 +0900

    update tests

--- tests/src/nlohmann_json_test.cpp
+++ tests/src/nlohmann_json_test.cpp
@@ -8,7 +8,7 @@ TEST_CASE("absolute_time_point json") {
     uint64_t u64 = 13835058055282163712ull; // 2^63 + 2^62
     pqrs::osx::chrono::absolute_time_point p1(u64);
     nlohmann::json json = p1;
-    pqrs::osx::chrono::absolute_time_point p2 = json;
+    auto p2 = json.get<pqrs::osx::chrono::absolute_time_point>();
     REQUIRE(p1 == p2);
     REQUIRE(json.dump() == "13835058055282163712");
   }
@@ -21,7 +21,7 @@ TEST_CASE("absolute_time_duration json") {
     int64_t i64 = 6917529027641081856ll; // 2^62 + 2^61
     pqrs::osx::chrono::absolute_time_duration d1(i64);
     nlohmann::json json = d1;
-    pqrs::osx::chrono::absolute_time_duration d2 = json;
+    auto d2 = json.get<pqrs::osx::chrono::absolute_time_duration>();
     REQUIRE(d1 == d2);
     REQUIRE(json.dump() == "6917529027641081856");
   }
@@ -30,7 +30,7 @@ TEST_CASE("absolute_time_duration json") {
     int64_t i64 = -6917529027641081856ll; // -(2^62 + 2^61)
     pqrs::osx::chrono::absolute_time_duration d1(i64);
     nlohmann::json json = d1;
-    pqrs::osx::chrono::absolute_time_duration d2 = json;
+    auto d2 = json.get<pqrs::osx::chrono::absolute_time_duration>();
     REQUIRE(d1 == d2);
     REQUIRE(json.dump() == "-6917529027641081856");
   }
