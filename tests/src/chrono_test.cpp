bc74973cc88456037367d728bccfbad9f776bfa8
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-28 17:59:44 +0900

    Use double to avoid overflow

--- tests/src/chrono_test.cpp
+++ tests/src/chrono_test.cpp
@@ -1,5 +1,5 @@
 #include <catch2/catch.hpp>
-
+#include <iostream>
 #include <pqrs/osx/chrono.hpp>
 
 TEST_CASE("make_absolute_time_duration") {
@@ -20,7 +20,7 @@ TEST_CASE("make_nanoseconds") {
   {
     pqrs::osx::chrono::absolute_time_duration absolute_time_duration(256 * 1000000);
     auto ns = pqrs::osx::chrono::make_nanoseconds(absolute_time_duration);
-    REQUIRE(pqrs::osx::chrono::make_absolute_time_duration(ns) == absolute_time_duration);
+    REQUIRE(std::abs(type_safe::get(pqrs::osx::chrono::make_absolute_time_duration(ns) - absolute_time_duration)) < 1000);
   }
 }
 
@@ -28,6 +28,6 @@ TEST_CASE("make_milliseconds") {
   {
     pqrs::osx::chrono::absolute_time_duration absolute_time_duration(256 * 1000000);
     auto ms = pqrs::osx::chrono::make_milliseconds(absolute_time_duration);
-    REQUIRE(pqrs::osx::chrono::make_absolute_time_duration(ms) == absolute_time_duration);
+    REQUIRE(std::abs(type_safe::get(pqrs::osx::chrono::make_absolute_time_duration(ms) - absolute_time_duration)) < 1000000);
   }
 }
